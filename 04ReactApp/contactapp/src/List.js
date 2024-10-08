b69c8b64edb4f184f441bd1be01362b558a3dfe5
Mike Shan <mike.shan.nz@gmail.com>
2024-10-08 16:11:09 +1300

    add files for contactapp

---
+++ 04ReactApp/contactapp/src/List.js
@@ -0,0 +1,45 @@
+const contacts = [
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+    },
+    {
+        id: 2,
+        firstName: 'Erica',
+        lastName: 'Doe',
+        email: 'ericadoe@example.com',
+    },
+    {
+        id: 3,
+        firstName: 'Jack',
+        lastName: 'Dommic',
+        email: 'jackdommic@example.com',
+    },
+];
+function List() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) => (
+                    <tr key={contact.id}>
+                        <td>{contact.id}</td>
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+export default List;
