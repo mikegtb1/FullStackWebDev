ac4a30ffe6d0418f680a57dffb0d1e2f895cad91
Mike Shan <mike.shan.nz@gmail.com>
2024-10-09 14:41:11 +1300

    update 04ReactApp

--- 04ReactApp/contactapp/src/List.js
+++ 04ReactApp/contactapp/src/List.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import ListItem from './ListItem';
+import Form from './Form';
+import './List.css';
 
 const initialContacts = [
     {
@@ -22,53 +24,26 @@ const initialContacts = [
     },
 ];
 
-
-var contacts2 = [
-    {
-        id: 1,
-        firstName: 'John222',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-    },
-    {
-        id: 2,
-        firstName: 'Erica222',
-        lastName: 'Doe',
-        email: 'ericadoe@example.com',
-    },
-    {
-        id: 3,
-        firstName: 'Jack222',
-        lastName: 'Dommic',
-        email: 'jackdommic@example.com',
-    },
-];
-
-var contacts3 = [];
-
 function List() {
 
     const [contacts, setContacts] = useState(initialContacts);
 
-    function handleDelete(contact) {
-
-        console.log('before delete');
-        console.log(contacts);
-
+    async function handleDelete(contact) {
 
         setContacts((prevState) =>
             prevState.filter((prevContact) => prevContact.id !== contact.id),);
-
-//        contacts.splice(contact.id - 1, 1);
-
-        console.log('after delete');
-        console.log(contacts);
-
-
     }
 
+    async function handleSave(contact) {
+            const id = contacts.length + 1;
+            setContacts((prevContacts) => [...prevContacts, { ...contact, id }]);
+        }
+        
+
     return (
-        <table>
+        <>
+        <h2 style={{ textDecoration: 'underline' }}>Contact list</h2>
+        <table className='contactTable'>
             <thead>
                 <tr>
                     <th>ID</th>
@@ -88,6 +63,8 @@ function List() {
                 ))}
             </tbody>
         </table>
+        <Form onSave={handleSave} />
+        </>
     );
 }
 export default List;
