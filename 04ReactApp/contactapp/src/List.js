fb6061387dade8966abeedbda2ce99370620b16b
Mike Shan <mike.shan.nz@gmail.com>
2024-10-09 13:49:29 +1300

    add more files to 04ReactApp

--- 04ReactApp/contactapp/src/List.js
+++ 04ReactApp/contactapp/src/List.js
@@ -1,4 +1,7 @@
-const contacts = [
+import { useState } from 'react';
+import ListItem from './ListItem';
+
+const initialContacts = [
     {
         id: 1,
         firstName: 'John',
@@ -18,7 +21,52 @@ const contacts = [
         email: 'jackdommic@example.com',
     },
 ];
+
+
+var contacts2 = [
+    {
+        id: 1,
+        firstName: 'John222',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+    },
+    {
+        id: 2,
+        firstName: 'Erica222',
+        lastName: 'Doe',
+        email: 'ericadoe@example.com',
+    },
+    {
+        id: 3,
+        firstName: 'Jack222',
+        lastName: 'Dommic',
+        email: 'jackdommic@example.com',
+    },
+];
+
+var contacts3 = [];
+
 function List() {
+
+    const [contacts, setContacts] = useState(initialContacts);
+
+    function handleDelete(contact) {
+
+        console.log('before delete');
+        console.log(contacts);
+
+
+        setContacts((prevState) =>
+            prevState.filter((prevContact) => prevContact.id !== contact.id),);
+
+//        contacts.splice(contact.id - 1, 1);
+
+        console.log('after delete');
+        console.log(contacts);
+
+
+    }
+
     return (
         <table>
             <thead>
@@ -27,16 +75,16 @@ function List() {
                     <th>First name</th>
                     <th>Last name</th>
                     <th>Email</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 {contacts.map((contact) => (
-                    <tr key={contact.id}>
-                        <td>{contact.id}</td>
-                        <td>{contact.firstName}</td>
-                        <td>{contact.lastName}</td>
-                        <td>{contact.email}</td>
-                    </tr>
+                    <ListItem
+                    key={contact.id}
+                    contact={contact}
+                    onDelete={handleDelete}
+                    />
                 ))}
             </tbody>
         </table>
---
+++ 04ReactApp/contactapp/src/ListItem.js
@@ -0,0 +1,12 @@
+function ListItem({ contact, onDelete }) {
+    return (
+        <tr>
+            <td>{contact.id}</td>
+            <td>{contact.firstName}</td>
+            <td>{contact.lastName}</td>
+            <td>{contact.email}</td>
+            <td><button onClick={() => onDelete(contact)}>Delete</button></td>
+        </tr>
+    );
+}
+export default ListItem;
